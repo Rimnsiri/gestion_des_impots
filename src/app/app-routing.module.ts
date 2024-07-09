e93aaa73507b67c9739b46c07591f1bc27e67715
Rimnsiri <nsiririm111@gmail.com>
2024-07-09 20:38:43 +0100

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
 import { AdminComponent } from './admin/admin.component';
 import { DashbordComponent } from './admin/dashbord/dashbord.component';
+import { UserComponent } from './admin/user/user.component';
 const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },  
   { path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) },
@@ -12,7 +13,8 @@ const routes: Routes = [
     component: AdminComponent,
     children: [
       { path: '', redirectTo: 'dashbord', pathMatch: 'full' },
-      { path: 'dashbord', component: DashbordComponent }
+      { path: 'dashbord', component: DashbordComponent },
+      { path: 'user', component: UserComponent }
     ]
   },
   { path: '**', redirectTo: '/login' },
